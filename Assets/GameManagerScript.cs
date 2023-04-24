aee8cb9dc6b26cc15d0177c467f37d19931733e6
功夢 佐藤 <k022g0117@g.neec.ac.jp>
2023-04-24 12:02:37 +0900

    a

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    int[] map;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
+        //Debug.Log("Hello world");
+        string debugText = "";
+        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        {
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText += map[i].ToString() + ",";
+                Debug.Log(map[i]+",");
+            }
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            int playerInbox = -1;
+            for(int i = 0; i < map.Length;i++)
+            {
+                if (map[i] == 1) 
+                {
+                    playerInbox = i;
+                    break;
+
+                }
+            }
+            if (playerInbox <map.Length - 1)
+            {
+                map[playerInbox + 1] = 1;
+                map[playerInbox] = 0;
+
+            }
+            string debugText = "";
+            for (int i = 0;i < map.Length;i++)
+            {
+                debugText += map[i].ToString() + ",";
+            }
+            Debug.Log(debugText);
+        }
     }
 }
