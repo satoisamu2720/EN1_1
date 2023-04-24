a9e87cf6b2d2d8d560a99ecbc6ac4767f8cae0b1
功夢 佐藤 <k022g0117@g.neec.ac.jp>
2023-04-24 11:30:42 +0900

    a

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -7,7 +7,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
